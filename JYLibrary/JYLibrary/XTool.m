424893ea254086e9cfad66f0f4ce927a08df4496
XJYao <xujiayao91@163.com>
2017-05-03 16:56:34 +0800

    邮箱格式

--- JYLibrary/JYLibrary/XTool.m
+++ JYLibrary/JYLibrary/XTool.m
@@ -116,7 +116,7 @@
 }
 
 + (BOOL)isValidateEmail:(NSString *)email {
-    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,10}";
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES%@",emailRegex];
     return [emailTest evaluateWithObject:email];
 }
