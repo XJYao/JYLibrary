c81768c56acdaae5911acc7a13e379c1a48e8362
XJY <xjy@XJYdeMac-mini.local>
2017-12-06 11:03:31 +0800

    iphonex适配

--- JYLibrary/JYLibrary/XMacro.h
+++ JYLibrary/JYLibrary/XMacro.h
@@ -71,6 +71,9 @@
 #define statusBarOriginY ((floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1) ? ([UIApplication sharedApplication].isStatusBarHidden ? 0.0f : statusBarHeight) : 0.0f) //因为是宏替换,而且?的优先级比较低,因此需要加个括号保证?先执行
 #define navigationBarHeight 44
 
+#define SafeAreaTopHeight ([UIScreen mainScreen].bounds.size.height == 812.0 ? 88 : 64)
+#define SafeAreaBottomHeight ([UIScreen mainScreen].bounds.size.height == 812.0 ? 34 : 0)
+
 /**
  tag的起始值
  */
